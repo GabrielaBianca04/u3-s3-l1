d94989a496fe3c05f68527b1fb1ffda37da95330
GabrielaBianca04 <143381847+GabrielaBianca04@users.noreply.github.com>
2023-12-11 19:00:51 +0100

    Add files via upload

---
+++ u3-s3-l1.sql
@@ -0,0 +1,6 @@
+INSERT INTO public."Prodotti"(
+	"idProdotto", "Descrizione", "inProduzione", "inCommercio", "dataAttivazione", "dataDisattivazione")
+	VALUES (1, "Profumo", "true", "true", 1/01/2020, 1/01/2025);
+	VALUES (2, "MP3", "true", "true", 1/01/2020, 1/01/2025);
+	VALUES (3, "Pasta", "true", "true", 1/01/2020, 1/01/2025);
+	
